544b5f284a2495655e92dff88f0721eebed6eaec
masongsong <masongsong@tuhu.cn>
2021-03-20 20:07:15 +0800

    fix: 改用在线主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,8 @@ const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
-  // theme: 'vdoing', // 使用依赖包主题
-  theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "松松的一尺三分地",
   description: 'web前端技术博客,专注于前端前沿技术领域',
