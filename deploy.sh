1fd67a65da18fda085f86acc93f7485ca84d6a34
masongsong <masongsong@tuhu.cn>
2021-04-03 10:25:12 +0800

    fix: 修改deploy脚本

--- deploy.sh
+++ deploy.sh
@@ -10,33 +10,33 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-# echo 'MssText.github.io.com' > CNAME
-# if [ -z "$GITHUB_TOKEN" ]; then
+echo 'MssText.github.io.com' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-#   githubUrl=git@github.com:MssText/MssText.github.io.git
-# else
-#   msg='来自github actions的自动部署'
-#   githubUrl=https://MssText:${GITHUB_TOKEN}@github.com/MssText/MssText.github.io.git
-#   git config --global user.name "masongsong"
-#   git config --global user.email "2698745548@qq.com"
-# fi
-# git init
-# git add -A
-# git commit -m "${msg}"
-# git push -f $githubUrl master:gh-pages # 推送到github
+  githubUrl=git@github.com:MssText/MssText.github.io.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://MssText:${GITHUB_TOKEN}@github.com/MssText/MssText.github.io.git
+  git config --global user.name "masongsong"
+  git config --global user.email "2698745548@qq.com"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
 # deploy to coding
 # echo 'www.MssText.github.io.com' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于gitee仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:mss-blog/mss-blog/blog.git
-else
-  codingUrl=https://mss-blog:${CODING_TOKEN}@e.coding.net/mss-blog/blog.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于gitee仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:mss-blog/mss-blog/blog.git
+# else
+#   codingUrl=https://mss-blog:${CODING_TOKEN}@e.coding.net/mss-blog/blog.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
 
 cd - # 退回开始所在目录
