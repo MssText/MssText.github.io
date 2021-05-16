984c28aef3db84be427f07cee639fcedc15fdf98
masongsong <masongsong@tuhu.cn>
2021-05-17 07:51:55 +0800

    feat: 增加《TypeScript》学习笔记

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,7 +2,7 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: '前端乱炖', 
+    text: '前端乱炖',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
@@ -23,7 +23,7 @@ module.exports = [
       {
         text: '学习笔记',
         items: [
-          { text: '《微前端》学习笔记', link: '/note/javascript/' }
+          { text: '《TypeScript》学习笔记', link: '/note/typescript/' }
         ],
       }
     ],
