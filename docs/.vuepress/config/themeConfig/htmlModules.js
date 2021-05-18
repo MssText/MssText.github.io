37a347a931bdb7d7f3bdfb85c12c43d39ad4e399
masongsong <masongsong@tuhu.cn>
2021-05-18 17:11:09 +0800

    feat: 增加TS标签

--- docs/.vuepress/config/themeConfig/htmlModules.js
+++ docs/.vuepress/config/themeConfig/htmlModules.js
@@ -19,17 +19,17 @@
  */
 
 module.exports = {
-  homeSidebarB:
-    `<!-- 纵向自适应 -->
-    <ins class="adsbygoogle"
-        style="display:block;padding: 0.95rem;"
-        data-ad-client="ca-pub-7828333725993554"
-        data-ad-slot="7802654582"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>`,
+//   homeSidebarB:
+//     `<!-- 纵向自适应 -->
+//     <ins class="adsbygoogle"
+//         style="display:block;padding: 0.95rem;"
+//         data-ad-client="ca-pub-7828333725993554"
+//         data-ad-slot="7802654582"
+//         data-ad-format="auto"
+//         data-full-width-responsive="true"></ins>
+//     <script>
+//         (adsbygoogle = window.adsbygoogle || []).push({});
+//     </script>`,
   // sidebarT:
   //   `<!--  固定100% * 150px可显示，max-height:150px 未见显示-->
   //   <ins class="adsbygoogle"
@@ -39,38 +39,38 @@ module.exports = {
   //     <script>
   //         (adsbygoogle = window.adsbygoogle || []).push({});
   //     </script>`,
-  sidebarB:
-    `<!-- 正方形 -->
-      <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="3508773082"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>`,
-  pageT:
-    `<!-- 固定100% * 90px可显示，max-height:90px未见显示-->
-     <ins class="adsbygoogle"
-          style="display:inline-block;width:100%;max-height:90px"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="6625304284"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>`,
+//   sidebarB:
+//     `<!-- 正方形 -->
+//       <ins class="adsbygoogle"
+//           style="display:block"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="3508773082"
+//           data-ad-format="auto"
+//           data-full-width-responsive="true"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>`,
+//   pageT:
+//     `<!-- 固定100% * 90px可显示，max-height:90px未见显示-->
+//      <ins class="adsbygoogle"
+//           style="display:inline-block;width:100%;max-height:90px"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="6625304284"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>`,
   // pageTshowMode: 'article',
-  pageB:
-    `<!-- 横向自适应 -->
-      <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="6620245489"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>`,
+//   pageB:
+//     `<!-- 横向自适应 -->
+//       <ins class="adsbygoogle"
+//           style="display:block"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="6620245489"
+//           data-ad-format="auto"
+//           data-full-width-responsive="true"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>`,
   // pageBshowMode: 'article',
   // windowLB: // 会遮挡部分侧边栏
   //   `<!-- 固定200*200px -->
@@ -82,16 +82,16 @@ module.exports = {
   //     <script>
   //         (adsbygoogle = window.adsbygoogle || []).push({});
   //     </script>`,
-  windowRB:
-     `<!-- 固定160*160px -->
-      <ins class="adsbygoogle"
-          style="display:inline-block;max-width:160px;max-height:160px"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="8377369658"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-      `,
+//   windowRB:
+//      `<!-- 固定160*160px -->
+//       <ins class="adsbygoogle"
+//           style="display:inline-block;max-width:160px;max-height:160px"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="8377369658"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>
+//       `,
 }
 
 
