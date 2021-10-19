b852b3d7c2d82c75a47b644ca402f1f13ccb28ce
masongsong <masongsong@tuhu.cn>
2021-10-19 11:44:10 +0800

    feat: 增加新文章

--- theme-vdoing/components/Buttons.vue
+++ theme-vdoing/components/Buttons.vue
@@ -102,11 +102,11 @@ export default {
           icon: 'icon-yejianmoshi',
           KEY: 'dark'
         },
-        {
-          name: '阅读模式',
-          icon: 'icon-yuedu',
-          KEY: 'read'
-        }
+        // {
+        //   name: '阅读模式',
+        //   icon: 'icon-yuedu',
+        //   KEY: 'read'
+        // }
       ],
       themeColorList: [
         {
